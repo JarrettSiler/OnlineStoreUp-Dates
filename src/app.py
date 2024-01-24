6957e7aec12ff11fad94d6d35d6d3626e7bfa633
Jarrett Siler <jarrettsiler@gmail.com>
2024-01-24 10:46:48 -0800

    First Commit

---
+++ src/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return '''
+     <form action="/echo_user_input" method="POST">
+         <input name="user_input">
+         <input type="submit" value="Submit!">
+     </form>
+     '''
+
+@app.route("/echo_user_input", methods=["POST"])
+def echo_input():
+    input_text = request.form.get("user_input", "")
+    return "You entered: " + input_text
+
+if __name__ == "__main__":
+    app.run(debug=True)
